ca6ce54c2b665f045096a70d88a12a940cfecec2
Terrance Kennedy <terrancerkennedy@gmail.com>
2018-02-08 00:24:59 -0700

    Second pass at lazy loading plugins

--- custom/lazy-nvm.zsh
+++ custom/lazy-nvm.zsh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
 
-source "$ZSH_CUSTOM/lib/lazy_load.sh"
+source "$ZSH_CUSTOM/lib/lazy_loader.sh"
 
 nvm_init(){
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
 }
 
-# create hooks for commands 'nvm', and 'node'
-lazy_load nvm_init nvm node
+# create hooks for commands shimmed by nvm
+lazy_load nvm_init nvm node eslint flow npm
--- custom/lazy-pyenv.zsh
+++ custom/lazy-pyenv.zsh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
-source "$ZSH_CUSTOM/lib/lazy_load.sh"
+source "$ZSH_CUSTOM/lib/lazy_loader.sh"
 
 pyenv_init() {
     export PYENV_INIT=true
     source "$ZSH/plugins/pyenv/pyenv.plugin.zsh"
 }
 
-lazy_load pyenv_init pyenv
+lazy_load pyenv_init pyenv 2to3 easy_install pip python python2 python3
